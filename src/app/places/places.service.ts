e9f0abbf402ddafc54f77be8b4416feed3b19eb8
Tuyen Pham <tuh39709@temple.edu>
2021-08-19 13:13:17 -0400

    create data services and output Discover Places

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PlacesService {
+
+  private placesArray: Place[] = [
+    new Place('p1','Manhattan Mansion', 'In the heart of new york city', 'https://i.ibb.co/wJh6hGB/new-york-city-3840x1600.jpg', 149),
+    new Place('p2','Cali', 'In the heart of new york city', 'https://i.ibb.co/wJh6hGB/new-york-city-3840x1600.jpg', 99),
+    new Place('p3','Philadelphia', 'In the heart of new york city', 'https://i.ibb.co/wJh6hGB/new-york-city-3840x1600.jpg', 99)
+  ];
+
+  get places(){
+    return [...this.placesArray];
+  }
+
+  constructor() { }
+}
