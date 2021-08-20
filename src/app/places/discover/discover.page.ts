55cda6dd837ade80b8660208448e9e15c484579d
Tuyen Pham <tuh39709@temple.edu>
2021-08-20 16:06:31 -0400

    implement virtual scrolling

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -12,11 +12,13 @@ import { PlacesService } from '../places.service';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(private placesService: PlacesService, private menuCtrl: MenuController) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 
   onOpenMenu() {
