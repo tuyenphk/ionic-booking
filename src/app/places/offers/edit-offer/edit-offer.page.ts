84d98ca88659d8d1377d3506be4d236cec49c444
Tuyen Pham <tuh39709@temple.edu>
2021-08-23 16:14:16 -0400

    edit the offer form

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
@@ -13,6 +14,7 @@ import { PlacesService } from '../../places.service';
 export class EditOfferPage implements OnInit {
 
   place: Place;
+  form: FormGroup;
   constructor(
     private route: ActivatedRoute,
     private navCtrl: NavController,
@@ -26,7 +28,24 @@ export class EditOfferPage implements OnInit {
         return;
       }
       this.place = this.placesService.places.find(p => p.id === paramMap.get('placeId'));
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        }),
+      });
     });
+  }
+
+  onUpdateOffer() {
+    if (!this.form.valid){
+      return;
+    }
+    console.log(this.form);
   }
 
 }
