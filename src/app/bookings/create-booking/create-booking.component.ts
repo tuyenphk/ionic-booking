4f15d2dc7f74c0c30cd0785fe67d5238a5561089
Tuyen Pham <tuh39709@temple.edu>
2021-08-23 16:56:23 -0400

    configuring the date controls

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MaxLengthValidator } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { Place } from 'src/app/places/place.model';
 
@@ -9,12 +10,28 @@ import { Place } from 'src/app/places/place.model';
 })
 export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place;
+  @Input() selectedMode: 'select' | 'random';
+  startDate: string;
+  endDate: string;
 
   constructor(
     private modalCtrl: ModalController
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+    if (this.selectedMode === 'random'){
+      this.startDate = new Date(
+        availableFrom.getTime() + Math.random()*(availableTo.getTime() - 7*24*60*60*1000 - availableFrom.getTime())
+      ).toISOString();
+
+      this.endDate = new Date(new Date(this.startDate).getTime() +
+        Math.random() *
+        (new Date(this.startDate).getTime() + 6*24*60*60*1000 - new Date(this.startDate).getTime())
+      ).toISOString();
+    }
+  }
 
   onBookPlace() {
     this.modalCtrl.dismiss({messgae: 'This is booking confirmation'}, 'confirm');
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -8,9 +8,33 @@ import { Place } from './place.model';
 export class PlacesService {
 
   private placesArray: Place[] = [
-    new Place('p1','Manhattan Mansion', 'In the heart of new york city', 'https://i.ibb.co/wJh6hGB/new-york-city-3840x1600.jpg', 999),
-    new Place('p2','Moxy NYC East Village', 'Small-but-smartly built rooms, airport-style check-ins, and no-frills service', 'https://i.ibb.co/fqqG4vK/moxy-east-village-check-in.jpg', 199),
-    new Place('p3','Hotel Central Park', 'Eco-conscious hotel', 'https://i.ibb.co/CtyFL4h/central-park-facade-at-1-hotel-central-park.jpg', 99)
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of new york city',
+      'https://i.ibb.co/wJh6hGB/new-york-city-3840x1600.jpg',
+      999,
+      new Date('2021-01-01'),
+      new Date('2030-12-31')
+    ),
+    new Place(
+      'p2',
+      'Moxy NYC East Village',
+      'Small-but-smartly built rooms, airport-style check-ins, and no-frills service',
+      'https://i.ibb.co/fqqG4vK/moxy-east-village-check-in.jpg',
+      199,
+      new Date('2021-01-01'),
+      new Date('2030-12-31')
+    ),
+    new Place(
+      'p3',
+      'Hotel Central Park',
+      'Eco-conscious hotel',
+      'https://i.ibb.co/CtyFL4h/central-park-facade-at-1-hotel-central-park.jpg',
+      99,
+      new Date('2021-01-01'),
+      new Date('2030-12-31')
+  )
   ];
 
   get places(){
